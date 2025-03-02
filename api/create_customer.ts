2f7db7702065d8df02eaebf6c71c156d6cf156f5
alegonzalezz <alegonzalez@dc.uba.ar>
2025-03-02 12:40:53 -0300

    fix process env vars

--- api/create_customer.ts
+++ api/create_customer.ts
@@ -1,5 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
